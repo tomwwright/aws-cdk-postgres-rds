e4760cd98db9dea1cdff098863014f3a4621ac3a
Tom Wright <tom.suffern.wright@gmail.com>
2023-05-17 16:03:07 +1000

    feat: blank cdk app

---
+++ test/aws-cdk-postgres-rds.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkPostgresRds from '../lib/aws-cdk-postgres-rds-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-postgres-rds-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkPostgresRds.AwsCdkPostgresRdsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
